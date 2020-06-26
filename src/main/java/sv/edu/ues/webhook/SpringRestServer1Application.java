ac0f8362563f174049e5d35e3bfdea7e7d2283dd
Steven Zaldivar <zh15002@ues.edu.sv>
2020-06-26 14:54:21 -0600

    Add project structure

---
+++ src/main/java/sv/edu/ues/webhook/SpringRestServer1Application.java
@@ -0,0 +1,13 @@
+package sv.edu.ues.webhook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestServer1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRestServer1Application.class, args);
+	}
+
+}
---
+++ src/test/java/sv/edu/ues/webhook/SpringRestServer1ApplicationTests.java
@@ -0,0 +1,13 @@
+package sv.edu.ues.webhook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestServer1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
